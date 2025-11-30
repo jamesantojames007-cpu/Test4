f628e72343c76f83dc2cbd06326978e8ad7e1959
jamesantojames007-cpu <@Jmaes1121>
2025-11-30 22:59:30 +0530

    simple commit

---
+++ Test.py
@@ -0,0 +1,10 @@
+def student_details(name, age):
+    print("Student Details:")
+    print("Name:", name)
+    print("Age:", age)
+    print("Welcome", name, "to the Python class!")
+
+# Main program
+student_name = "James"
+student_age = 20
+student_details(student_name, student_age)
